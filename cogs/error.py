a547a571574d9a20df517b8878dd4fad9dbbbc81
Darshan L Arekal <darshanlarekal@gmail.com>
2022-05-08 00:23:25 +0530

    fix ephemeral

--- cogs/error.py
+++ cogs/error.py
@@ -4,12 +4,12 @@ from discord.ext import commands
 
 class error(commands.Cog):
 	def __init__(self , bot):
-		self.bot = bot 
+		self.bot = bot
 
 	@commands.Cog.listener()
 	async def on_application_command_error(self , ctx , error):
-		raise error 
-		await ctx.respond(error)
+		raise error
+		await ctx.respond(error ,ephemeral = True)
 
 def setup(bot):
 	bot.add_cog(error(bot))
