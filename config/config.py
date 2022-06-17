0b7fe25d0bcb38a7db6a111a7c10c44d72cab8c8
Darshan L Arekal <darshanlarekal@gmail.com>
2022-06-17 21:41:07 +0530

    eEeeeE

--- config/config.py
+++ config/config.py
@@ -1,5 +1,6 @@
 import os
 
 BOT_TOKEN : str = os.getenv("BOT_TOKEN")
-MONGO_USERNAME : str = os.getenv('MONGO_USERNAME')
-MONGO_PASSWORD : str = os.getenv('MONGO_PASSWORD')
+# MONGO_USERNAME : str = os.getenv('MONGO_USERNAME')
+# MONGO_PASSWORD : str = os.getenv('MONGO_PASSWORD')
+GUILDIDS = [914830703974305842, 973423250682023966]
